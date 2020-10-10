14b0d0c37d0833ce4091f2350396327dac0021bf
developermanish <manishsrivastava1078@gmail.com>
2020-10-10 14:52:17 +0530

    backend configured

---
+++ server/data.js
@@ -0,0 +1,44 @@
+export default {
+    foods: [{
+        _id:'1',
+        name: 'Chicken Masala',
+        category: 'Non-Veg',
+        image: '/images/f1.jpg',
+        price: 60,
+        hotelName: 'Maharaja',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        _id:'2',
+        name: 'Mutton Masala',
+        category: 'Non-Veg',
+        image: '/images/f1.jpg',
+        price: 80,
+        hotelName: 'Maharaja',
+        rating: 4.8,
+        numReviews: 20
+    },
+    {
+        _id:'3',
+        name: 'Panner Tikka',
+        category: 'Veg',
+        image: '/images/f1.jpg',
+        price: 40,
+        hotelName: 'Maharaja',
+        rating: 4.2,
+        numReviews: 15
+    },
+    {
+        _id:'4',
+        name: 'Murg Musalam',
+        category: 'Non-Veg',
+        image: '/images/f1.jpg',
+        price: 80,
+        hotelName: 'Maharaja',
+        rating: 4.6,
+        numReviews: 25
+    },
+
+    ]
+}
